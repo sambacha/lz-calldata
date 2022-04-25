9a7c4f085111e4254651675f59f419add162f749
sam bacha <sam@manifoldfinance.com>
2022-04-25 06:31:47 -0700

    compileOutput

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
 const fs = require('fs'); // Built-in dependency for file streaming.
 const solc = require('solc'); // Our Solidity compiler
-
-// Import the Web3 library at the top of your file
 const Web3 = require('web3');
 
 
-const content = fs.readFileSync('test.sol', 'utf-8'); // Read the file...
+const content = fs.readFileSync('lz.t.sol', 'utf-8');
 
 // Format the input for solc compiler:
 const input = {
   language: 'Solidity',
   sources: {
-    'test.sol' : {
+    'lz.t.sol' : {
       "content": content // The imported content
     }
   },
@@ -33,15 +31,15 @@ const provider = new Web3.providers.HttpProvider("http://localhost:8545");
 const web3 = new Web3(provider);
 
 // Get the compiled contract's abi (interface)
-const Contract = output.contracts["test.sol"];
+const Contract = output.contracts["lz.t.sol"];
 
 
-const { abi, evm } = Contract.Test // We'll use the "evm" variable later
+const { abi, evm } = Contract.Test
 
 // Initialize a new contract object:
 const contract = new web3.eth.Contract(abi);
 
-console.log("Done compiling");
+console.log("compiled output");
 
 function deployAndRunContract() {
 	web3.eth.getAccounts().then(function(addresses) {
@@ -57,17 +55,17 @@ function deployAndRunContract() {
 			.on('error', function(err) {
 				console.log("Failed to deploy:", err) 
 			}).then(function(newContract) {
-				console.log("Deployed exploitable contract");
+				console.log("[WARN]: exploitable contract");
 				newContract.methods.ext().call({
 					from: addresses[1],
 					gas: 1000000,
 					gasPrice,
 				}).then(function(receipt) {
-					console.log("and we get back " + receipt);
+					console.log("[receipt]: we get back " + receipt);
 				});
 			})
 		})
 	})
 }
 
-deployAndRunContract(); // Call the function
+deployAndRunContract();
